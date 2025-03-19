344335cc82f3f3ea4a787a99a81d14a6f77256e3
Dhanush <142816727+HegdeDhanush@users.noreply.github.com>
2025-03-19 18:30:02 +0530

    Create newfile.cpp

---
+++ newfile.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, This is PES2UG22CS174\n";
+  cout << "I have successfully done with jenkins lab\n";
+  return 0;
+}
